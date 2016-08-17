77d02790637ec83c672b55585b56b6d34787a9d2
Micael Pimentel <micael.pimentel1@gmail.com>
2016-08-16 20:55:50 -0400

    Update debounce.vhd

--- debounce.vhd
+++ debounce.vhd
@@ -1,29 +1,32 @@
+-- Módulo debounce de chave em VHDL
+-- Micael Pimentel
+-- github.com/altron42/vhdl-codes
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity debounce is
    generic(
-	   fclk : integer := 1;   -- frequancia do clock
-      twindow : integer := 10   -- tamamnho da janela de clock que o sinal devera permanecer o mesmo para ser valido
+      twindow : integer := 50   -- quantidades de pulso de clock que o sinal deverá permaner o mesmo para ficar estável. Ajustar conforme sua aplicação
 	);
    port (
-	   x : in std_logic;
-      clk : in  std_logic; 
-      y : buffer  std_logic
+	   x : in std_logic;  -- Sinal de entrada
+      clk : in  std_logic;  -- Clock
+      y : buffer  std_logic  -- Sinal debounceado
 	);
 end debounce;
 
 architecture debouncer of debounce is
-   constant max : integer := fclk * twindow;
+   constant max : integer := twindow;
 begin
    process (clk)
 	   variable count : integer range 0 to max;
    begin
       if rising_edge(clk) then
-         if x /= y then
+         if x /= y then  -- Sinal de entrada está variando
             count := count + 1;
-				if count = max then
+				if count = max then  -- sinal estabilizou e a contagem atingiu o valor máximo
 				   y <= x;
 					count := 0;
 				end if;
