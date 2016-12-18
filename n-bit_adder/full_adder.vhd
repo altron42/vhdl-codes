d08cb103adec1ef06dc0982dbaf77666f6dec510
Micael Pimentel <micael.pimentel1@gmail.com>
2016-12-18 10:49:25 -0400

    Update full_adder.vhd

--- n-bit_adder/full_adder.vhd
+++ n-bit_adder/full_adder.vhd
@@ -8,15 +8,15 @@ entity full_adder is
    port (
 	   carry_in : in std_logic;
 	   data_A : in std_logic;
-		data_B : in std_logic;
+	   data_B : in std_logic;
 	   sum : out std_logic;
-		carry_out : out std_logic
+	   carry_out : out std_logic
 	);
 end full_adder;
 
 architecture hardware of full_adder is
 begin
    sum <= data_A xor data_B xor carry_in;
-	carry_out <= (data_A and data_B) or (data_A and carry_in) or (data_B and carry_in);
+   carry_out <= (data_A and data_B) or (data_A and carry_in) or (data_B and carry_in);
 	
 end hardware;
